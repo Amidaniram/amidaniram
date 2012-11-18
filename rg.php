031f479f89943906457423be1373153554bc9818
Amidaniram <amidaniram@yandex.ru>
2012-11-19 02:30:58 +0400

    4th commit

---
+++ rg.php
@@ -0,0 +1,16 @@
+<html>
+
+	<head>
+	<title>Страница входа</title>
+	</head>
+
+	<body>
+		<form action="registration.php"method="GET"> 
+		<p>Имя<input type="text" name="nickname" value=""/></p>
+		<p>Пароль<input type="password" name="password" value=""/></p>
+		<input type="submit" name="registration" value="Зарегестрироваться"/></br>
+	</body>
+
+</html>
+	
+	
